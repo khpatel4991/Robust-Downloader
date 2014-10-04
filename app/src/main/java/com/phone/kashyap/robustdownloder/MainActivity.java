7d02616bf4afba90cb3009cc17814feaa44ee030
khpatel4991 <khpatel4991@gmail.com>
2014-10-03 21:56:24 -0400

    First Commit.
    Using Async Task, HTTPConn class

--- app/src/main/java/com/phone/kashyap/robustdownloder/MainActivity.java
+++ app/src/main/java/com/phone/kashyap/robustdownloder/MainActivity.java
@@ -20,6 +20,10 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.NotificationCompat.WearableExtender;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -153,39 +157,22 @@ public class MainActivity extends Activity
 			protected void onPostExecute(Void aVoid)
 			{
 				super.onPostExecute(aVoid);
-				Toast.makeText(getActivity(), _fileName + " downloaded!!", Toast.LENGTH_SHORT).show();
+				_notificationManager.cancel(NOTIFICATION_ID);
+				//Toast.makeText(getActivity(), _fileName + " downloaded!!", Toast.LENGTH_SHORT).show();
 			}
 
 			private void createNotification(String contentTitle, String contentText)
 			{
-				/*NotificationCompat.Builder mBuilder =
-						new NotificationCompat.Builder(this)
-								.setSmallIcon(R.drawable.ic_launcher)
-								.setContentTitle(contentTitle)
-								.setContentText(contentText);
-				// Creates an explicit intent for an Activity in your app
-				Intent resultIntent = new Intent(getActivity(), MainActivity.class);
 
-				// The stack builder object will contain an artificial back stack for the
-				// started Activity.
-				// This ensures that navigating backward from the Activity leads out of
-				// your application to the Home screen.
-				TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-				// Adds the back stack for the Intent (but not the Intent itself)
-				stackBuilder.addParentStack(ResultActivity.class);
-				// Adds the Intent that starts the Activity to the top of the stack
-				stackBuilder.addNextIntent(resultIntent);
-				PendingIntent resultPendingIntent =
-						stackBuilder.getPendingIntent(
-								0,
-								PendingIntent.FLAG_UPDATE_CURRENT
-						);
-				mBuilder.setContentIntent(resultPendingIntent);
-				NotificationManager mNotificationManager =
-						(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-				// mId allows you to update the notification later on.
-				mNotificationManager.notify(mId, mBuilder.build());*/
+				NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity());
+				builder.setAutoCancel(true);
+				builder.setContentTitle(contentTitle);
+				builder.setContentText(contentText);
+				builder.setSmallIcon(R.drawable.ic_launcher);
 
+				_notification = builder.build();
+				_notificationManager = (NotificationManager) getActivity().getSystemService(NOTIFICATION_SERVICE);
+				_notificationManager.notify(NOTIFICATION_ID, _notification);
 			}
 		}
 
